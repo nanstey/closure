35b30f6b933b5f0387f4a706c9d64ee44e706179
Noel Anstey <anstey.ng@gmail.com>
2017-03-09 15:42:19 -0800

    added exercise code

---
+++ dice.js
@@ -0,0 +1,20 @@
+var rollDie = function () {
+  return Math.floor(1 + Math.random() * 6);
+}
+
+console.log(rollDie());  // 1 (for example)
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+  return function() {
+    /* your code here */
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
